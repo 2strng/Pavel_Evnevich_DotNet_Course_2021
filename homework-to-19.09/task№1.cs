9f573b019281d642913b3f5b2bb3952e58a11f25
2strng <90731539+2strng@users.noreply.github.com>
2021-09-19 23:12:33 +0300

    Add files via upload

---
+++ homework-to-19.09/task№1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа №1");
+
+            Console.Write("Введите переменную a = ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите переменную b = ");
+
+            int y = int.Parse(Console.ReadLine());
+
+            Swap(ref x,ref y);
+
+        }
+
+
+        public static void Swap(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.Write($"Переменная a = {a}, переменная b = {b} ");
+        }
+
+        
+
+    }
+}
---
+++ homework-to-19.09/task№2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = -1;
+            bool repeat = true;
+            {
+
+
+                Console.WriteLine("Задание №2");
+
+                Console.Write("Введите число(string)");
+
+                string input = Console.ReadLine();
+
+                try
+                {
+                    num = Convert.ToInt32(input);
+                    if (num < Int32.MaxValue)
+                    {
+                        Console.WriteLine("Значение числа - {0}" , +num);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Значение num не может быть увеличено");
+                    }
+                }
+                catch (FormatException)
+                {
+
+                }
+
+            }
+
+        }
+    }
+}
